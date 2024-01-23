9cba3c2109ca00b4f59ad157fa94b007a72ad9d5
Bonevy BEBY <bonevybeby@gmail.com>
2024-01-23 08:32:25 +0100

    added: counter//setState//list elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+
+class App extends React.Component {
+  state = {
+    clients: [
+      { id: 1, name: "Bonevy BEBY" },
+      { id: 1, name: "Bonevy BEBY" },
+      { id: 1, name: "Bonevy BEBY" },
+
+    ],
+    counter: 0
+  }
+  handleClick = () => {
+    this.setState({ counter: this.state.counter + 1 })
+    console.log(this.state)
+  }
+  render() {
+    const title = "List of clients";
+    return (
+      <div>
+        <h1> {title}</h1>
+        <ul>
+          {this.state.counter}
+          <button onClick={this.handleClick}>click me</button>
+          {this.state.clients.map((client, index) => <li key={index}>{client.name}<button>x</button></li>)}
+        </ul>
+        <form>
+          <input type="text" placeholder="enter your name" />
+          <button>Confirmer</button>
+        </form>
+      </div>
+
+    );
+  }
+}
+
+export default App;
+
+
+
+
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+      </div>
+    );
+  }
 
-export default App;
+export default App; */
+
+
